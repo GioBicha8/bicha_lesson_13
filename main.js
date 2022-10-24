d2196829f4849b3da6b5fe7724bb1086ee26ba9c
GioBicha8 <g.bicha888@gmail.com>
2022-10-24 16:01:47 +0400

    all done

---
+++ main.js
@@ -0,0 +1,65 @@
+// console.log("hello from js file");
+
+let myname = "giorgi";
+let myage = 24;
+let yearOfBirth = 1998;
+let myhobby = "Biliards";
+let currentYear = "2022";
+let isstudent = true;
+let isemployed = false;
+let myUndefined;
+const myNull = null;
+
+console.log(typeof myname, myname);
+console.log(typeof myage, myage);
+console.log(typeof yearOfBirth, yearOfBirth);
+console.log(typeof currentYear, currentYear);
+console.log(typeof myhobby, myhobby);
+console.log(typeof isstudent, isstudent);
+console.log(typeof isemployed, isemployed);
+console.log(typeof myUndefined, myUndefined);
+console.log(typeof myNull, myNull);
+
+let num1 = 10;
+let num2 = 30;
+let num3 = 15;
+let num4 = 4;
+let text = "text";
+
+const substraction = num2 - num1;
+const multiplication = num3 * num4;
+const devision = num4 / text;
+const modulo = num2 % num4; //ნაშთი
+const istrue = num1 < 30;
+const power = num1 ** num4;
+
+console.log(
+  typeof substraction,
+  multiplication,
+  devision,
+  modulo,
+  istrue,
+  power
+);
+
+const add1 = num1 + num2;
+const add2 = num1 + num2 + text;
+const add3 = text + num1 + num2;
+
+console.log(
+  add1,
+  "num1 + num2",
+  add2,
+  "num1 + num2 + text",
+  add3,
+  "text + num1 + num2"
+);
+
+const name = "my name is Giorgi";
+let num5 = 34;
+let num6 = 10;
+const age = "im";
+const agee = "years old";
+const substractio = num5 - num6;
+const hobby = "and my hobby is plaing Billiards";
+console.log(name, age, substractio, agee, hobby);
