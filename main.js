351676cd8fd236ee8884eac50fe32e8f85289433
GioBicha8 <g.bicha888@gmail.com>
2022-10-24 16:24:52 +0400

    all done _!

--- main.js
+++ main.js
@@ -20,46 +20,42 @@ console.log(typeof isemployed, isemployed);
 console.log(typeof myUndefined, myUndefined);
 console.log(typeof myNull, myNull);
 
-let num1 = 10;
-let num2 = 30;
-let num3 = 15;
-let num4 = 4;
-let text = "text";
+let text1 = "my name is Giorgi,";
+let text2 = "im 24 yeats old";
+let text3 = "im from Georgia.";
+let text4 = "i was born in Tbilisi in 1998";
+let text5 = "my hobbie is plaing billiards ";
+let text6 = "and plaing Football";
 
-const substraction = num2 - num1;
-const multiplication = num3 * num4;
-const devision = num4 / text;
-const modulo = num2 % num4; //ნაშთი
-const istrue = num1 < 30;
-const power = num1 ** num4;
+// // const substraction = num2 - num1;
+// // const multiplication = num3 * num4;
+// const devision = num4 / text;
+// const modulo = num2 % num4; //ნაშთი
+// const istrue = num1 < 30;
+// const power = num1 ** num4;
 
-console.log(
-  typeof substraction,
-  multiplication,
-  devision,
-  modulo,
-  istrue,
-  power
-);
+// console.log(
+//   typeof substraction,
+//   multiplication,
+//   devision,
+//   modulo,
+//   istrue,
+//   power
+// );
 
-const add1 = num1 + num2;
-const add2 = num1 + num2 + text;
-const add3 = text + num1 + num2;
+const add1 = text1 + text2;
+const add2 = text3 + text4;
+const add3 = text5 + text6;
 
-console.log(
-  add1,
-  "num1 + num2",
-  add2,
-  "num1 + num2 + text",
-  add3,
-  "text + num1 + num2"
-);
+console.log(add1, add2, add3);
 
-const name = "my name is Giorgi";
+const name = "HI my name is Giorgi";
+const born = "i was born in Georgia,Tbilisi";
 let num5 = 2022;
 let num6 = 1998;
 const age = "im";
 const agee = "years old";
 const substractio = num5 - num6;
-const hobby = "and my hobby is plaing Billiards";
-console.log(name, age, substractio, agee, hobby);
+const hobby = "and my hobby is plaing Billiards.";
+
+console.log(name, born, age, substractio, agee, hobby);
